24610aa0095b1ad75f1d634c7d413caeec044b18
Ben Jones <benpaddlejones@outlook.com>
2024-03-10 15:08:37 +1100

    add challenge 4 solution

--- Challenge4/Challenge4.ino
+++ Challenge4/Challenge4.ino
@@ -7,13 +7,36 @@ Written by:
 Date: 
 
 *********************************************/
-#include "AIDriver.h"
-	
-	void setup(){   // sets up once as the program starts    
-    //Instantiate a new two wheeled robot to the pointer you created above
-
+  #include "AIDriver.h"
+  
+  AIDriver *mrJonesDriving;
+ 	
+	void setup(){   // sets up once as the program starts
+    //Instantiate a two wheeled to the pointer 'mrJonesDriving'
+    mrJonesDriving = new AIDriver();
+    driveOneMeter(); //this will call the function
 	}
 
 	void loop(){ // loops continuously 50 times a second
 
 	}
+
+  void driveOneMeter () {
+      mrJonesDriving->driveForward(130,125);
+      delay(2000);
+      mrJonesDriving->brake();
+      delay(300);
+      mrJonesDriving->rotateRight(200);
+      delay(410);
+      mrJonesDriving->brake();
+      delay(300);
+      mrJonesDriving->driveForward(130,125);
+      delay(2000);
+      mrJonesDriving->brake();
+      delay(300);
+}
+
+/* Extension notes 
+ - Move the function call from the setup to the loop, first predict then test
+ - Then see how few lines of code you can write to make it go up and back constantly.
+ - Progressively accelerate and/or deaccelerate my robot to full speed.
